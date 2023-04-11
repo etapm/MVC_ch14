8c56095c89f9e92af2df5408fa994d4c1bc0877c
etapm <etap@ucdavis.edu>
2023-04-11 16:25:45 -0700

    models update

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/connection");
+
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// Define relationships
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Post, Comment };
