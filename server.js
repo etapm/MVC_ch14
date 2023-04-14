dd326608f5528271949294d6bca7453cfeb2ce4b
etapm <etap@ucdavis.edu>
2023-04-14 15:23:41 -0700

    fixing css connection issue

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,16 +15,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-app.use(
-  "/css",
-  express.static(__dirname + "../public/css", {
-    setHeaders: function (res, path) {
-      res.setHeader("Content-Type", mime.getType(path));
-    },
-  })
-);
+app.use(express.static(path.join(__dirname, "public")));
 
 const sessionConfig = {
   secret: process.env.SESSION_SECRET,
