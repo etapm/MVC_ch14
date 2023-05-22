928cd3d3e7d99919bad02e4bac944c338f6508a6
etapm <etap@ucdavis.edu>
2023-05-22 13:47:46 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -16,10 +16,13 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "database_production",
-    host: "127.0.0.1",
+    use_env_variable: "DATABASE_URL",
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
